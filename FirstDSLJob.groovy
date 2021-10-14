30abfb0043e068b57702aeb216485092f3b905c5
Kartik <kartik9555@gmail.com>
2021-10-14 22:01:30 +0200

    Updated file

--- FirstDSLJob.groovy
+++ FirstDSLJob.groovy
@@ -1,5 +1,5 @@
 job('Maven_First_DSL_Job') {
-    description('First maven job genenrated by DSL on ${new Date()}, the project is a small maven project hosted on Github')
+    description('First maven job genenrated by DSL on ${new Date()}, the project is a small maven project hosted on Git')
     scm {
         git('https://github.com/Kartik9555/springboot-unit-example.git', 'master')
     }
