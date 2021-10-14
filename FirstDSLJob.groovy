fa7622419812ec18d7328befa95712a31f9e03a1
Kartik <kartik9555@gmail.com>
2021-10-14 19:49:42 +0200

    Updated file

---
+++ FirstDSLJob.groovy
@@ -0,0 +1,15 @@
+job('Maven_First_DSL_Job') {
+    description('First maven job genenrated by DSL on ${new Date()}, the project is a small maven project hosted on Github')
+    scm {
+        github('https://github.com/Kartik9555/springboot-unit-example.git', 'master')
+    }
+    triggers {
+        scm(* * * * *)
+    }
+    steps {
+        maven('clean package')
+    }
+    publishers {
+        archiveArtifacts('**/*.jar')
+    }
+}
