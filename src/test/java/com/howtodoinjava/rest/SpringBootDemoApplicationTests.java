b224de70d40e208f81b793e8d613ac1800942c8b
Kartik <kartik9555@gmail.com>
2021-10-14 22:50:52 +0200

    Updated file

--- src/test/java/com/howtodoinjava/rest/SpringBootDemoApplicationTests.java
+++ src/test/java/com/howtodoinjava/rest/SpringBootDemoApplicationTests.java
@@ -13,7 +13,6 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
